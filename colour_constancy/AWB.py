db5b89e7df4ce795f4c1beb2a30b95e986a490a7
Irene Papadopoulou <irenepap3@gmail.com>
2021-09-18 18:30:19 +0200

    Created AWB.py
    Finished code for part 4.

---
+++ colour_constancy/AWB.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+def grey_world(img):
+    img = ((img * (img.mean() / img.mean(axis=(0, 1))))
+             .clip(0, 255).astype(int))
+    return img
+
+
+if __name__ == '__main__':
+    original_img = cv2.imread('./colour_constancy/What-Are-Plants.jpg')
+    original_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2RGB)
+
+    img_after_gw = grey_world(original_img)
+
+    fig = plt.figure()
+    fig.add_subplot(1, 2, 1)
+    plt.imshow(original_img)
+    plt.title("Original")
+    plt.axis('off')
+    fig.add_subplot(1, 2, 2)
+    plt.imshow(img_after_gw)
+    plt.title("Colour corrected")
+    plt.axis('off')
+    plt.show()
