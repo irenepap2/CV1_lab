Fixed the bug for the wrong dimensions (height and width)

photometric/estimate_alb_nrm.py
@@ -40,15 +40,15 @@ def estimate_alb_nrm( image_stack, scriptV, shadow_trick=True):
         normal at this point is g / |g|
     """
 
-    for x in range(w):
-        for y in range(h):
+    for x in range(h):
+        for y in range(w):
             i = image_stack[x,y,:]
             
             if shadow_trick:
                 scriptI = np.diag(i)
             else:
                 scriptI = np.eye(n)
-
+            scriptV = np.nan_to_num(scriptV)
             g, _, _, _ = np.linalg.lstsq(np.matmul(scriptI,scriptV),np.matmul(scriptI,i),rcond=None)
             
             albedo[x,y] = np.linalg.norm(g)

photometric/photometric_stereo.py
@@ -74,28 +74,28 @@ def photometric_stereo(image_dir='./SphereGray5/', isRGB = False):
         # show results
         show_results_RGB(albedos, normals_all_channels, height_map_all_channels, SE_all_channels)
 
-    ## Face
-    def photometric_stereo_face(image_dir='./yaleB02/'):
-        [image_stack, scriptV] = load_face_images(image_dir)
-        [h, w, n] = image_stack.shape
-        print('Finish loading %d images.\n' % n)
-        print('Computing surface albedo and normal map...\n')
-        albedo, normals = estimate_alb_nrm(image_stack, scriptV)
+## Face
+def photometric_stereo_face(image_dir='./yaleB02/'):
+    [image_stack, scriptV] = load_face_images(image_dir)
+    [h, w, n] = image_stack.shape
+    print('Finish loading %d images.\n' % n)
+    print('Computing surface albedo and normal map...\n')
+    albedo, normals = estimate_alb_nrm(image_stack, scriptV, False)
 
-        # integrability check: is (dp / dy  -  dq / dx) ^ 2 small everywhere?
-        print('Integrability checking')
-        p, q, SE = check_integrability(normals)
+    # integrability check: is (dp / dy  -  dq / dx) ^ 2 small everywhere?
+    print('Integrability checking')
+    p, q, SE = check_integrability(normals)
 
-        threshold = 0.005
-        print('Number of outliers: %d\n' % np.sum(SE > threshold))
-        SE[SE <= threshold] = float('nan') # for good visualization
+    threshold = 0.005
+    print('Number of outliers: %d\n' % np.sum(SE > threshold))
+    SE[SE <= threshold] = float('nan') # for good visualization
 
-        # compute the surface height
-        height_map = construct_surface( p, q )
+    # compute the surface height
+    height_map = construct_surface( p, q , 'column')
 
-        # show results
-        show_results(albedo, normals, height_map, SE)
+    # show results
+    show_results(albedo, normals, height_map, SE)
     
 if __name__ == '__main__':
-    photometric_stereo('./photometric/photometrics_images/MonkeyColor/', isRGB=True)
-    # photometric_stereo_face()
+    # photometric_stereo('./photometric/photometrics_images/SphereGray25/', isRGB=False)
+    photometric_stereo_face('./photometric/photometrics_images/yaleB02/')

photometric/utils.py
@@ -81,7 +81,7 @@ def load_face_images(image_dir='./yaleB02/'):
     
 def show_results(albedo, normals, height_map, SE):
     # Stride in the plot, you may want to adjust it to different images
-    stride = 8
+    stride = 1
     
     # showing albedo map
     fig = plt.figure()
@@ -108,16 +108,16 @@ def show_results(albedo, normals, height_map, SE):
     np.arange(1))
     X = X[..., 0]
     Y = Y[..., 0]
-    Z = height_map[::stride,::stride]
+    Z = height_map[::stride,::stride].T
     '''
     =============
     You could further inspect the shape of the objects and normal directions by using plt.quiver() function.  
     =============
     '''
     fig = plt.figure().add_subplot(projection='3d')
-    U = normals[::stride,::stride, 0]
-    V = normals[::stride,::stride, 1]
-    W = normals[::stride,::stride, 2]
+    U = normals[::stride,::stride, 0].T
+    V = normals[::stride,::stride, 1].T
+    W = normals[::stride,::stride, 2].T
     plt.quiver(X, Y, Z, U, V, W, length=0.1, normalize=True)
     plt.show()
 