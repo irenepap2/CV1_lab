4847e90cc49d1550511c3692f2274a3a71f45e15
Irene Papadopoulou <irenepap3@gmail.com>
2021-09-24 12:40:12 +0200

    Minor changes

--- lab2/Image_enhancement/compute_LoG.py
+++ lab2/Image_enhancement/compute_LoG.py
@@ -3,6 +3,7 @@ import cv2
 import matplotlib.pyplot as plt
 from gauss2D import *
 from scipy import signal
+import scipy.ndimage as nd
 
 def log2D(sigma, kernel_size):
     LoG = np.zeros((kernel_size, kernel_size))
@@ -11,8 +12,9 @@ def log2D(sigma, kernel_size):
 
     for x_i in x:
         for y_i in y:
-            LoG[np.where(x == x_i), np.where(y == y_i)] = -1/(np.pi * sigma **4) * (1 - (x_i**2 + y_i**2)/(2*sigma**2))*(np.exp(-(x_i**2+y_i**2)/(2*sigma**2)))
+            LoG[np.where(x == x_i), np.where(y == y_i)] = -1/(np.pi * sigma**4) * (1 - (x_i**2 + y_i**2)/(2*sigma**2))*(np.exp(-(x_i**2+y_i**2)/(2*sigma**2)))
     
+    print(LoG)
     #normalize
     LoG = LoG/np.sum(LoG)
     return LoG
@@ -22,29 +24,32 @@ def compute_LoG(image, LOG_type):
     if LOG_type == 1:
         #method 1
         G = gauss2D(0.5, 0.5, 5)
-        G = signal.convolve2d(image, G, mode='same')
-        imOut = cv2.Laplacian(G, ddepth=-1)
+        G = signal.convolve2d(image, G, boundary='fill', mode='same')
+        laplacian = np.array([[0, 1, 0], [1, -4, 1], [0, 1 , 0]])
+        imOut = signal.convolve2d(image, laplacian, boundary='fill', mode='same')
         return imOut
 
     elif LOG_type == 2:
         #method 2
-        LoG = log2D(0.5, 5)
-        imOut = signal.convolve2d(image, LoG, mode='same')
+        #LoG = log2D(0.5, 5)
+        #LoG = nd.gaussian_laplace(image, 0.5)
+        LoG = np.array([[0, 0, 1, 0, 0], [0, 1, 2, 1, 0], [1, 2, -16, 2 , 1], [0, 1, 2, 1, 0], [0, 0, 1, 0, 0]])
+        imOut = signal.convolve2d(image, LoG, boundary='fill', mode='same')
         return imOut
 
     elif LOG_type == 3:
         #method 3
         G1 = gauss2D(0.5, 0.5, 5)
-        G2 = gauss2D(1, 1, 5)
+        #1.6 times σ1
+        G2 = gauss2D(0.8, 0.8, 5)
         DoG = G1 - G2
-        imOut = signal.convolve2d(image, DoG, mode='same')
+        imOut = signal.convolve2d(image, DoG, boundary='fill', mode='same')
         return imOut
-
 
 if __name__ == '__main__':
     im = cv2.imread('./lab2/Image_enhancement/images/image2.jpg', 0)
 
-    imOut = compute_LoG(im, LOG_type=2)
+    imOut = compute_LoG(im, LOG_type=3)
 
     figure = plt.figure()
     plt.imshow(imOut, cmap="gray")
