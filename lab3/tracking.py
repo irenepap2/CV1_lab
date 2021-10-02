26f3bedc31ca99f287f61c40d4eb2d85f31d7d99
Irene Papadopoulou <irenepap3@gmail.com>
2021-10-02 15:37:38 +0200

    Video tracking

--- lab3/tracking.py
+++ lab3/tracking.py
@@ -7,21 +7,21 @@ from harris_corner_detector import *
 from lucas_kanade import calculate_optical_flow_with_LK_for_corners
 import os
 
-def save_video(video_name, frames):
-    height, width = frames[0].shape
+def save_video(video_name, frames, width, height):
     video = cv2.VideoWriter(video_name, 0, 1, (width,height))
 
     for frame in frames:
         video.write(frame)
 
 def update_corners(r, c, Vx, Vy):
-    r += Vy.astype(np.int)
-    c += Vx.astype(np.int)
+    r = np.round(r + Vy).astype(np.int)
+    c = np.round(c + Vx).astype(np.int)
     return r, c
 
 def tracking(path):
     files = os.listdir(path)
     img = cv2.imread(os.path.join(path, files[0]), 0)
+    W, H = img.shape
     # detect corners of first image
     H, r, c = harris_corner_detector(img, 1, 3, 0.001, 5, plot=False)
     figure = plt.figure()
@@ -38,7 +38,7 @@ def tracking(path):
         frame = np.frombuffer(figure.canvas.tostring_rgb(), dtype=np.uint8)
         frames.append(frame)
 
-    save_video("output.avi", frames)
+    save_video("output.avi", frames, W, H)
 
 
 if __name__ == '__main__':
